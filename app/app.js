3e7e9dc14364cd759f6f4bb3fae6d70811d05da1
Edward Aryee <edwardaaryee@gmail.com>
2016-02-09 21:46:24 -0500

    Added favicon

--- app/app.js
+++ app/app.js
@@ -5,11 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'ui.bootstrap',
 
-  'myApp.version'
-  
   'myApp.view1',
   'myApp.view2',
-
+  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/directives/header/header.js
+++ app/directives/header/header.js
@@ -1,11 +1,12 @@
-module.directive('header', function () {
+angular.module('myApp')
+  .directive('header', function () {
     return {
-        restrict: 'A', //This means that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
-        replace: true,
-        scope: {}, // This is one of the cool things :). Will be explained in post.
-        templateUrl: "directives/header.html",
-        controller: ['$scope', '$filter', function ($scope, $filter) {
-            // Your behaviour goes here :)
-        }]
+      restrict: 'A', //This means that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+      replace: true,
+      scope: {}, // This is one of the cool things :). Will be explained in post.
+      templateUrl: "directives/header.html",
+      controller: ['$scope', '$filter', function ($scope, $filter) {
+          // Your behaviour goes here :)
+      }]
     }
 });
